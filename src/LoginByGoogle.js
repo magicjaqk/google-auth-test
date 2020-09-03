be2e51cc22669719048a6b9f18385d73368204e7
magicjaqk <magicjaqk@gmail.com>
2020-09-03 13:22:59 -0600

    first commit

---
+++ src/LoginByGoogle.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { Redirect } from 'react-router-dom';
+
+const LoginByGoogle = () => {
+    let [signInBanner, setSignBanner] = useState('Please Sign In');
+
+    const responseGoogle = (response) => {
+        console.log(response.isSignedIn ? true : false);
+        if (response.isSignedIn) {
+            setSignBanner(`Welcome ${response.profileObj.givenName}!`);
+        }
+        //let signInStatus = response.isSignedIn ? true : false;
+        //window.sessionStorage.setItem('isSignedIn', signInStatus);
+        //window.sessionStorage.setItem('userData', JSON.stringify(response.profileObj));
+        
+    };
+
+    const logoutGoogle = (response) => {
+        console.log('Logout Success');
+        setSignBanner('Please Sign In');
+    };
+
+    return (
+        <div className="container-fluid row">
+            <div className="col-md-3" />
+            <div className="col-md-6 h-100 py-5">
+                <div className="card text-center my-auto bg-light">
+                    <div className="card-body">
+                        <h1 className="card-title text-dark">{signInBanner}</h1>
+                        <hr style={{ 'backgroundColor': '#343A40'}} />
+                        <span className="mr-3">
+                            <GoogleLogin
+                                clientId="730712262782-d2m3u2mn76s2fasgsniv1d45723a9qli.apps.googleusercontent.com"
+                                buttonText="Login with Google"
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                                cookiePolicy={'single_host_origin'}
+                                isSignedIn={true}
+                                theme="dark"
+                            />
+                            {/* uxMode="redirect" for redirect rather than popup */}
+                        </span>
+                        <span>
+                            <GoogleLogout 
+                                clientId="730712262782-d2m3u2mn76s2fasgsniv1d45723a9qli.apps.googleusercontent.com"
+                                buttonText="Logout"
+                                onLogoutSuccess={logoutGoogle}
+                                onFailure={logoutGoogle}
+                                theme="dark"
+                            />
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3" />
+        </div>
+    );
+};
+
+export default LoginByGoogle;
